2981afd9c303a3c4b037e0cba8651e5fed43faf5
Brian Lee <75905365+btlee264855@users.noreply.github.com>
2023-09-15 23:29:33 -0400

    camera functional

--- interpreter.py
+++ interpreter.py
@@ -2,30 +2,6 @@ import mediapipe as mp
 import numpy as np
 import cv2 as cv
 
-def cap1():
-   import numpy as np
-import cv2 as cv
-cap = cv.VideoCapture(-1)
-if not cap.isOpened():
- print("Cannot open camera")
- exit()
-while True:
- # Capture frame-by-frame
- ret, frame = cap.read()
- # if frame is read correctly ret is True
- if not ret:
-  print("Can't receive frame (stream end?). Exiting ...")
- break
- # Our operations on the frame come here
- gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
- # Display the resulting frame
- cv.imshow('frame', gray)
- if cv.waitKey(1) == ord('q'):
-  break
-# When everything done, release the capture
-cap.release()
-cv.destroyAllWindows()
-
 BaseOptions = mp.tasks.BaseOptions
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
@@ -45,7 +21,26 @@ with GestureRecognizer.create_from_options(options) as recognizer:
   # ...
 
   # Use OpenCV’s VideoCapture to start capturing from the webcam.
-  cap1()
+  cam = cv.VideoCapture(0)
+  if not cam.isOpened():
+    print("error opening camera")
+    exit()
+  while True:
+    # Capture frame-by-frame
+    ret, frame = cam.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("error in retrieving frame")
+        break
+    img = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    cv.imshow('frame', img)
+
+    
+    if cv.waitKey(1) == ord('q'):
+        break
+
+  cam.release()
+  cv.destroyAllWindows()
   # Create a loop to read the latest frame from the camera using VideoCapture#read()
 
   # Convert the frame received from OpenCV to a MediaPipe’s Image object.
