feda9fea1bb336dd6648b2af2433a7cfcbcaa646
Brian Lee <75905365+btlee264855@users.noreply.github.com>
2023-09-16 12:45:49 -0400

    update model trainer

--- model.py
+++ model.py
@@ -1,15 +1,16 @@
-
 import matplotlib.pyplot as plt
 import seaborn as sns
+import keras
 from keras.models import Sequential
 from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout , BatchNormalization
 from keras.preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,confusion_matrix
+from keras.callbacks import ReduceLROnPlateau
 import pandas as pd
 
-train_df = pd.read_csv("./sign_mnist_train.csv")
-test_df = pd.read_csv("./sign_mnist_test.csv")
+train_df = pd.read_csv("sign_mnist_train.csv")
+test_df = pd.read_csv("sign_mnist_test.csv")
 
 y_train = train_df['label']
 y_test = test_df['label']
@@ -31,19 +32,21 @@ x_train = x_train.reshape(-1,28,28,1)
 x_test = x_test.reshape(-1,28,28,1)
 
 datagen = ImageDataGenerator(
-        featurewise_center=False,
-        samplewise_center=False, 
-        featurewise_std_normalization=False,
-        samplewise_std_normalization=False,
-        zca_whitening=False,
-        rotation_range=10,
-        zoom_range = 0.1, 
-        width_shift_range=0.1,
-        height_shift_range=0.1,
-        horizontal_flip=False,
-        vertical_flip=False)
+        featurewise_center=False,  # set input mean to 0 over the dataset
+        samplewise_center=False,  # set each sample mean to 0
+        featurewise_std_normalization=False,  # divide inputs by std of the dataset
+        samplewise_std_normalization=False,  # divide each input by its std
+        zca_whitening=False,  # apply ZCA whitening
+        rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1, # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=False,  # randomly flip images
+        vertical_flip=False)  # randomly flip images
 
 datagen.fit(x_train)
+
+learning_rate_reduction = ReduceLROnPlateau(monitor='val_accuracy', patience = 2, verbose=1,factor=0.5, min_lr=0.00001)
 
 model = Sequential()
 model.add(Conv2D(75 , (3,3) , strides = 1 , padding = 'same' , activation = 'relu' , input_shape = (28,28,1)))
@@ -60,10 +63,9 @@ model.add(Flatten())
 model.add(Dense(units = 512 , activation = 'relu'))
 model.add(Dropout(0.3))
 model.add(Dense(units = 24 , activation = 'softmax'))
-
 model.compile(optimizer = 'adam' , loss = 'categorical_crossentropy' , metrics = ['accuracy'])
 model.summary()
 
-history = model.fit(datagen.flow(x_train,y_train, batch_size = 128) ,epochs = 20 , validation_data = (x_test, y_test))
+history = model.fit(datagen.flow(x_train,y_train, batch_size = 128) ,epochs = 20 , validation_data = (x_test, y_test) , callbacks = [learning_rate_reduction])
 
 model.save('smnist.h5')
